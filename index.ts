163726f02bfc91c95b4dcb54caaca13dfaec0ace
Milkshift <99212308+Milkshiift@users.noreply.github.com>
2024-03-10 18:37:28 +0200

    Delete types.d.ts

--- index.ts
+++ index.ts
@@ -2,7 +2,34 @@ import * as fs from 'fs';
 import { dirname, isAbsolute, resolve } from 'path';
 import { promisify } from 'util';
 
-import { Config, Stats, TreeItem } from './types';
+export interface Config {
+    /** GitHub token for authorization in private repositories */
+    token?: string;
+
+    /** Max number of async requests at the same time. 10 by default.
+     * download-directory.github.io has no limit, but it can lead to IP blocking
+     */
+    requests?: number;
+
+    /** Disable console logs */
+    muteLog?: boolean;
+}
+
+export interface TreeItem {
+    path: string;
+    mode: string;
+    type: string;
+    sha: string;
+    size: number;
+    url: string;
+}
+
+export interface Stats {
+    files: Record<string, string>;
+    downloaded: number;
+    success: boolean;
+    error?: any;
+}
 
 const streamPipeline = promisify(require('stream').pipeline);
 
